3b00d92f652ed819496256f68b59774072c5853d
subhom11 <subhomkundu@gmail.com>
2019-09-06 18:54:10 +0530

    Updates

---
+++ precache-manifest.06d1a2b9fd3f951b9705c7207a76cbb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80cb91d5a7b19b59da6163f56eb2f67",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ec3751b90f9f34f5db54",
+    "url": "/react-gh-pages/static/css/main.fc107c0e.chunk.css"
+  },
+  {
+    "revision": "6e84cf2ce5810b6fae07",
+    "url": "/react-gh-pages/static/js/2.be27b5ad.chunk.js"
+  },
+  {
+    "revision": "ec3751b90f9f34f5db54",
+    "url": "/react-gh-pages/static/js/main.5342003e.chunk.js"
+  },
+  {
+    "revision": "cd60c3dada059fda633e",
+    "url": "/react-gh-pages/static/js/runtime~main.f2d17e16.js"
+  }
+]);
