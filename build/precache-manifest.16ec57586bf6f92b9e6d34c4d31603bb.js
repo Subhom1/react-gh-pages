8c77094b92322a5337bb452a17b041cca75a0435
subhom11 <subhomkundu@gmail.com>
2019-09-06 17:24:21 +0530

    `

---
+++ build/precache-manifest.16ec57586bf6f92b9e6d34c4d31603bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1b5273f5cb4899fac38dc3b36b350d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f3d0e823edc6ab0ee95c",
+    "url": "/react-gh-pages/static/css/main.e82a11c3.chunk.css"
+  },
+  {
+    "revision": "6e84cf2ce5810b6fae07",
+    "url": "/react-gh-pages/static/js/2.be27b5ad.chunk.js"
+  },
+  {
+    "revision": "f3d0e823edc6ab0ee95c",
+    "url": "/react-gh-pages/static/js/main.5342003e.chunk.js"
+  },
+  {
+    "revision": "cd60c3dada059fda633e",
+    "url": "/react-gh-pages/static/js/runtime~main.f2d17e16.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.e3fd8ab5f5e4dbc09f6c930116dd0ff2.js"
+  "/react-gh-pages/precache-manifest.16ec57586bf6f92b9e6d34c4d31603bb.js"
 );
 
 self.addEventListener('message', (event) => {
