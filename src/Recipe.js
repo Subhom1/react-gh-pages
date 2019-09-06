83899c0daf92ce040b77dcd68e2a5f6ef0971acf
subhom11 <subhomkundu@gmail.com>
2019-09-06 22:26:19 +0530

    updated all

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,10 +7,10 @@ const Recipe = ({ title, calories, image, ingredients }) => {
         <div className="d-flex flex-column align-items-center justify-content-center m-4 bg-white recipecard">
             <h1>{title}</h1>
             <h5>{calories.toFixed(2)} Cal</h5>
-            <ol>{ingredients.map(ingredient => (
+            <ul className="unorderedlist">{ingredients.map(ingredient => (
                 <li className="text-dark small px-4">{ingredient.text}</li>)
             )}
-            </ol>
+            </ul>
             <img src={image} alt="" />
         </div>
     );
