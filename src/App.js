2aaa70a5f39b7f511a2c2d8ad2c292f2565c73be
subhom11 <subhomkundu@gmail.com>
2019-09-07 03:28:58 +0530

    ```

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ const App = () => {
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder='Search any dish . . .' />
         <button className="search-button" type="submit">Search</button>
+        <h6 className="cf">Made with <img src={"logo192.png"} className='imgbox'></img>React</h6>
       </form>
 
       <div className="row crow">
